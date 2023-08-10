63432a168646089f3d8c5aa78c9c2c080c5c4a04
Ekeshwar Jhode <mayurjhode55@gmail.com>
2023-08-10 10:35:49 +0530

    Employee Models created

--- companyApi/api/models.py
+++ companyApi/api/models.py
@@ -16,3 +16,19 @@ class Company(models.Model):
                              ))
     addedDate = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+#Creating Employee Model
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=50,choices=(
+        ('Manager','manager'),
+        ('SDE-2','sde-2'),
+        ('SDE-1','sde-1'),
+        ('Application Engineer','AE')
+    ))
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
--- companyApi/api/serializers.py
+++ companyApi/api/serializers.py
@@ -3,6 +3,7 @@ from api.models import Company
 
 #Create Serializers here
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    company_id = serializers.ReadOnlyField()
     class Meta:
         model = Company
         fields = "__all__"
